e82d55c13479e96d54e8d0ddd8d5c732bf0b0883
Matheus <matheus.malmeida7@gmail.com>
2022-01-25 16:58:58 -0300

    Criando model de Post

---
+++ src/main/java/com/tech/desafio/model/Post.java
@@ -0,0 +1,87 @@
+package com.tech.desafio.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_post")
+public class Post {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String title;
+	
+	@NotNull
+	private String description;
+	
+	@NotNull
+	private String body;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date created_at = new Date(System.currentTimeMillis());
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date updated_at;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public Date getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(Date created_at) {
+		this.created_at = created_at;
+	}
+
+	public Date getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(Date updated_at) {
+		this.updated_at = updated_at;
+	}
+	
+	
+
+}
